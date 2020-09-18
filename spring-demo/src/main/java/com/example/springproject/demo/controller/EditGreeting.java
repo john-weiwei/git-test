7ed62035840ae7de7d9683c38d8a9fc645d73468
zhangweiwei <2128093477@qq.com>
2020-09-18 18:57:57 +0800

    添加spring注解

---
+++ spring-demo/src/main/java/com/example/springproject/demo/controller/EditGreeting.java
@@ -0,0 +1,14 @@
+package com.example.springproject.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+/**
+ * @author ZhangWeiWei
+ * @date 2020/9/18 18:38
+ * @description
+ */
+@Controller
+@SessionAttributes("greeting") //
+public class EditGreeting {
+}
