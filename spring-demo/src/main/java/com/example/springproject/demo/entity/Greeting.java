a2d0865221cdba6232b79a001b49a0b93bf66821
zhangweiwei <2128093477@qq.com>
2020-09-27 18:32:59 +0800

    新增文件

--- spring-demo/src/main/java/com/example/springproject/demo/entity/Greeting.java
+++ spring-demo/src/main/java/com/example/springproject/demo/entity/Greeting.java
@@ -1,13 +1,43 @@
 package com.example.springproject.demo.entity;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * @author ZhangWeiWei
  * @date 2020/9/17 17:55
  * @description
  */
 public class Greeting {
+
+    @NotBlank
     private int id;
+
+    @NotBlank
+    @Size(min = 5,max = 15)
     private String content;
+
+    @AssertFalse
+    private boolean isShow;
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", isShow=" + isShow +
+                '}';
+    }
+
+    public boolean isShow() {
+        return isShow;
+    }
+
+    public void setShow(boolean show) {
+        isShow = show;
+    }
 
     public int getId() {
         return id;
--- spring-demo/src/test/java/com/example/springproject/demo/DemoApplicationTests.java
+++ spring-demo/src/test/java/com/example/springproject/demo/DemoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.springproject.demo;
 
+import com.example.springproject.demo.entity.Greeting;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+
+        int a = 0;
+        System.out.println("运行test");
     }
+
+    @Test
+    void test() {
+        String str = null;
+        AtomicInteger atomicInteger = new AtomicInteger();
+        Greeting greeting = new Greeting(atomicInteger.incrementAndGet(),"hi");
+        System.out.println(greeting.toString());
+        greeting.setShow(true);
+        System.out.println(greeting.toString());
+
+    }
+
 
 }
