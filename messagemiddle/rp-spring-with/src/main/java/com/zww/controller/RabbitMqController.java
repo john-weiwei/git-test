570d1cad7752ac6995311fc16abdd64047648fd6
unitimes <Allen@X-MacBook-Pro.local>
2020-11-09 19:42:16 +0800

    新增spring

---
+++ messagemiddle/rp-spring-with/src/main/java/com/zww/controller/RabbitMqController.java
@@ -0,0 +1,13 @@
+package com.zww.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author:ZhangWeiWei
+ * @Date:2020/11/9
+ * @Description:
+ * 消息生产者
+ */
+@Controller
+public class RabbitMqController {
+}
