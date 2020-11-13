af658d0e0a65c98ab3957edebfa3653e18c022ce
unitimes <Allen@X-MacBook-Pro.local>
2020-11-13 21:19:56 +0800

    新增springboot项目

---
+++ messagemiddle/rp-springboot-with/src/main/java/com/cn/zww/config/RabbitConfig.java
@@ -0,0 +1,137 @@
+package com.cn.zww.config;
+
+import com.cn.zww.constant.RmConst;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @Author:ZhangWeiWei
+ * @Date:2020/11/13
+ * @Description:
+ * rabbitmq 配置
+ */
+@Component
+public class RabbitConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.username}")
+    private String userName;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.port}")
+    private String port;
+
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
+
+    @Value("${spring.rabbitmq.publisher-confirms}")
+    private boolean publisherConfirm;
+
+    /**
+     * 构建连接工厂
+     * 使用 amqp包下的连接工厂
+     * @return
+     */
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses(host+":"+port);
+        connectionFactory.setUsername(userName);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost(virtualHost);
+
+        // TODO: 2020/11/13 发送方消息确认
+        connectionFactory.setPublisherConfirms(publisherConfirm);
+        return connectionFactory;
+    }
+
+    /**
+     * RabbitAdmin 封装对rabbitmq的操作
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    /**
+     * RabbitTemplate
+     * @return
+     */
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMandatory(true);
+        // TODO: 2020/11/13 发送方确认
+        rabbitTemplate.setConfirmCallback(confirmCallback());
+        // TODO: 2020/11/13 失败通知
+        rabbitTemplate.setReturnCallback(returnCallback());
+        return rabbitTemplate;
+    }
+
+    // TODO: 2020/11/13 发送方确认
+    /**
+     * 发送方确认回调函数
+     * @return
+     */
+    @Bean
+    public RabbitTemplate.ConfirmCallback confirmCallback() {
+        return (correlationData, ack, cause) -> {
+            if (ack) {
+                System.out.println(">>>发送方确认消息mq成功");
+            } else {
+                System.out.println(">>>>>发送方发送消息失败，考虑重发"+cause);
+            }
+        };
+    }
+
+    // TODO: 2020/11/13 失败通知
+    /**
+     * 失败通知
+     * @return
+     */
+    @Bean
+    public RabbitTemplate.ReturnCallback returnCallback() {
+        return (message, replyCode, replyText, exchange, routingKey) -> {
+            System.out.println("无法路由的消息");
+            System.out.println("ReplyText : "+replyText);
+            System.out.println("Reply Exchange : "+exchange);
+            System.out.println("Reply Code : "+replyCode);
+            System.out.println("RoutingKey : "+routingKey);
+            String msg = new String(message.getBody());
+            System.out.println("Message : "+msg);
+        };
+    }
+
+    // 声明队列------使用RabbitMQ缺省交换器（TopicExchange）
+
+    /**
+     * 声明队列最简单的方式
+     * @return
+     */
+    @Bean
+    public Queue helloQueue() {
+        return new Queue(RmConst.QUEUE_HELLO);
+    }
+
+    @Bean
+    public Queue userQueue() {
+        return new Queue(RmConst.QUEUE_USER);
+    }
+
+
+
+}
